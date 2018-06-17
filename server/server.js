41c09a368c564fd3cd5fa33e2dc8393dd37cb185
Gagan <gagan_gupta@live.in>
2018-06-17 17:29:40 +0530

    added individual get for todo item

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let bodyParser = require('body-parser');
 
+let {ObjectId} = require('mongodb');
 let {mongoose} = require('./db/mongoose');
 let {Todo} = require('./models/todo');
 let {User} = require('./models/user');
@@ -29,6 +30,22 @@ app.get('/todos', (req, res) => {
     }, (e) => {
         res.status(400).send(e);
     })
+});
+
+//get a specific item
+app.get('/todos/:id', (req, res) => {
+   let id = req.params.id;
+
+   if(!ObjectId.isValid(id)){
+       return res.status(404).send({'error': 'Invalid ID'});
+   }
+
+   Todo.findById(id).then((todo) => {
+       if(!todo){
+          return res.status(404).send({'error': 'Not found'});
+       }
+       res.send(todo);
+   }).catch((e) => res.status(404).send(e));
 });
 
 app.listen(3000, () => {
